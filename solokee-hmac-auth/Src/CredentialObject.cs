107b6fc522fb9a39eefabeba5a4d44676bf17d43
Fabian Spieß <fubezz@googlemail.com>
2020-04-26 18:16:24 +0200

    working key creation

---
+++ solokee-hmac-auth/Src/CredentialObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SoloKee
+{
+    class CredentialObject
+    {
+
+        private string credId;
+        public string CredentialID { 
+            get {
+                return credId;
+            } 
+            set
+            {
+                if (value == null) throw new ArgumentNullException("value");
+                credId = value;
+            } 
+        }
+
+        private string key;
+        public string Key {
+            get
+            {
+                return key;
+            }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("value");
+                key = value;
+            }
+        }
+
+        private string challenge;
+        public string Challenge
+        {
+            get
+            {
+                return challenge;
+            }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("value");
+                challenge = value;
+            }
+        }
+    }
+}
